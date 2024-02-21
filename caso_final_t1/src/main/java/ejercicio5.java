2409b08c31b7e045471b1dde056674ecc84d70c9
vitaliy-pg <aljndrvt@myuax.com>
2024-02-21 11:28:25 +0100

    creo el ejercicio 5

---
+++ caso_final_t1/src/main/java/ejercicio5.java
@@ -0,0 +1,24 @@
+class NavegadorEstelar {
+    private int[][] terrain; // Matrix to represent the terrain
+
+    public NavegadorEstelar(int[][] terrain) {
+        this.terrain = terrain;
+    }
+
+    public void calculateRoutes() {
+        // TODO: Implement route calculation logic
+    }
+
+    public void visualizeRoutes() {
+        // TODO: Implement route visualization logic
+    }
+
+    public void planConstructions() {
+        // TODO: Implement construction planning logic
+    }
+
+    public int[][] multiplyMatrices(int[][] matrix1, int[][] matrix2) {
+        // TODO: Implement matrix multiplication logic
+        return null;
+    }
+}
